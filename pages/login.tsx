9e017ddd186141e48386302694931f73f4bd145a
test <pk0820@cu.ac.kr>
2024-08-27 16:49:45 +0900

    0827 자동로그인을 위한 cookie 작업

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import LoginBox from "../components/login/loginBox";
 import { LoginContainer } from "../styles/LoginStyle";
+import { useCookies } from "react-cookie";
 
 function LoginPage() {
+  const [cookie, setCookie] = useCookies(["accessKey"]);
+  useEffect(() => {
+    console.log("cookie :>> ", cookie.accessKey);
+    return () => {};
+  }, []);
   return (
     <LoginContainer>
       <LoginBox />
